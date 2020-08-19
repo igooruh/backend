477ee9be6f3dd4dd9f8f3a57cf47ccf5befdea11
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-08-18 22:49:52 -0300

    Add query params, route params and request body

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+
+    const { title, student } = request.query;
+    console.log(title);
+    console.log(student);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -12,6 +19,10 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+
+    const body = request.body;
+    console.log(body);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -22,6 +33,11 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+
+    const id = request.params;
+
+    console.log(id);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
